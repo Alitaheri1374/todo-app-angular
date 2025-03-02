ba05c541437a2a7f0886d388937f9f153b075db0
Ali Taheri <taheri.aliTaheri.ali@gmail.com>
2025-03-02 15:43:50 +0330

    v2

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,12 +10,32 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
   imports:[MatSlideToggleModule],
 })
 export class HeaderComponent implements OnInit {
-  constructor() { }
+  name;
+  id;
+  isToggle;
+
+  constructor() {
+     this.name= "Ali Taheri 0120";
+     this.id= "elementId";
+     this.isToggle= false;
+  }
 
   ngOnInit(): void { }
 
   toggleMenu(){
+    this.isToggle=true;
+    this.name="yes that right";
+    console.log(this.id);
     console.log("toggleMenu Click");
   }
+
+  nameFun(){
+    return this.isToggle?this.name:`hello ${this.name}`;
+  }
+
+  mouseHandller(e:MouseEvent){
+    alert(`${e.detail.valueOf}`)
+  }
+
 }
 
